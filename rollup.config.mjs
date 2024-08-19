ac9b019ae36191f6e9ee17b28b996520dd766d31
Utyfua <utyfua@gmail.com>
2024-08-19 12:07:44 +0000

    updated deps with build pipeline and added some tests

---
+++ rollup.config.mjs
@@ -0,0 +1,54 @@
+
+import nodeResolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import { babel } from '@rollup/plugin-babel';
+
+import pkg from './package.json' assert { type: 'json' };
+
+const banner = `
+/*!
+ * ${pkg.name} v${pkg.version} by ${pkg.author}
+ * ${"homepage" in pkg ? pkg.homepage : `https://github.com/${pkg.repository}`}
+ * @license ${pkg.license}
+ */
+`.trim()
+
+const defaultExportOutro = `
+  module.exports = exports.default || {}
+  Object.entries(exports).forEach(([key, value]) => { module.exports[key] = value })
+`
+
+export default {
+    input: 'src/index.ts',
+    output: [
+        {
+            file: pkg.main,
+            format: 'cjs',
+            sourcemap: true,
+            exports: 'named',
+            outro: defaultExportOutro,
+            banner
+        },
+        {
+            file: pkg.module,
+            format: 'es',
+            sourcemap: true,
+            exports: 'named',
+            banner
+        }
+    ],
+    external: [
+        ...Object.keys(pkg.dependencies || {}),
+    ],
+    plugins: [
+        nodeResolve(),
+        babel({
+            exclude: 'node_modules/**',
+            babelHelpers: 'runtime'
+        }),
+        commonjs(),
+        // Compile TypeScript files
+        typescript(),
+    ],
+};
---
+++ test/page.spec.js
@@ -0,0 +1,43 @@
+const { default: puppeteer } = require('puppeteer');
+const { CookieJar } = require('tough-cookie');
+const { getStoreByPage } = require('../');
+
+describe('CookieJar async operations', () => {
+    /**
+     * @param {import('puppeteer').Browser} 
+     */
+    let browser;
+    /**
+     * @type {CookieJar}
+     */
+    let jar;
+
+    beforeEach(async () => {
+        browser = await puppeteer.launch({
+            headless: true,
+            args: [
+                '--incognito',
+                '--start-in-incognito',
+            ],
+        });
+
+        const page = await browser.newPage();
+
+        jar = new CookieJar(await getStoreByPage(page));
+    });
+
+    test('should set and get a cookie asynchronously', async () => {
+        const url = 'https://example.com';
+        const cookieStr = 'testcookie=value';
+
+        await jar.setCookie(cookieStr, url);
+        const cookies = await jar.getCookies(url);
+
+        expect(cookies).toHaveLength(1);
+        expect(cookies[0].key + '=' + cookies[0].value).toBe(cookieStr);
+    });
+
+    afterEach(async () => {
+        await browser.close();
+    })
+});
